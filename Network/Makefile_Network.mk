d1a74f71d7e55ea5ab8a6ce458d7096359ab22b3
PerAlban <alban.peralta@epitech.eu>
2024-05-27 04:04:52 +0200

    :white_check_mark: Create tests for the linked lists

--- Network/Makefile_Network.mk
+++ Network/Makefile_Network.mk
@@ -31,9 +31,10 @@ CFLAGS	=	$(INCLUDE) $(WARNINGS) #$(VALGRIND)
 
 #-------------- Tests Variables --------------#
 
-TESTS_SRC		=	mainTests.c
+TESTS_SRC		=	mainTest.c						\
+					ClientList/tests_linked_list.c	\
 
-TESTS_TRUE_SRC	=	$(patsubst %,Tests/src/%, $(TEST_SRC))	\
+TESTS_TRUE_SRC	=	$(patsubst %,Tests/src/%, $(TESTS_SRC)) \
 					$(filter-out src/main.c, $(TRUE_SRC))
 
 TESTS_FLAGS		=	--coverage -lcriterion
@@ -62,11 +63,17 @@ clean:
 
 fclean:	clean
 	rm -f $(NAME)
+	rm -f unit_tests*
+	rm -f *.gc*
+	rm -f vgcore*
+	rm -f *.so
 	printf "\033[1;35mBinary removed ✅\033[0m\n"
 
 re: fclean all
 
 tests_compile: fclean
+	$(info $(TESTS_TRUE_SRC))
+	@echo $(TESTS_TRUE_SRC)
 	gcc -o unit_tests $(TESTS_TRUE_SRC) $(CFLAGS) $(TESTS_FLAGS)
 	@if [ -f unit_tests ]; then \
 		printf "\033[1;32mTests compiled ✅\033[0m\n"; \
