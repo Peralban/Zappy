f4289536329b9fd12b81d769ad1334b0560cbdff
PerAlban <alban.peralta@epitech.eu>
2024-05-29 05:12:14 +0200

    :zap: Improve makefile to don't print unwanted things

--- Network/Makefile_Network.mk
+++ Network/Makefile_Network.mk
@@ -64,7 +64,7 @@ $(NAME):	$(OBJ)
 
 clean:
 	rm -f $(OBJ)
-	printf "\033[1;35mObject files removed ✅\033[0m\n"
+	@printf "\033[1;35mObject files removed ✅\033[0m\n"
 
 fclean:	clean
 	rm -f $(NAME)
@@ -72,7 +72,7 @@ fclean:	clean
 	rm -f *.gc*
 	rm -f vgcore*
 	rm -f *.so
-	printf "\033[1;35mBinary removed ✅\033[0m\n"
+	@printf "\033[1;35mBinary removed ✅\033[0m\n"
 
 re: fclean all
 
@@ -88,10 +88,10 @@ tests_compile: fclean
 
 tests_launch:
 	./unit_tests
-	printf "\033[1;35mTests launched ✅\033[0m\n"
+	@printf "\033[1;35mTests launched ✅\033[0m\n"
 	gcovr --exclude tests/
 	gcovr --exclude tests/ --branches
-	printf "\033[1;35mCoverage generated ✅\033[0m\n"
+	@printf "\033[1;35mCoverage generated ✅\033[0m\n"
 
 tests_run: tests_compile tests_launch
-	printf "\033[1;32mTests runned ✅\033[0m\n"
+	@printf "\033[1;32mTests runned ✅\033[0m\n"
