cfdce648a9aa2f5da1f3b16391545a6885ec1e56
julien <julien.renard@epitech.eu>
2024-05-31 08:26:36 +0200

    :sparkles: add empty method in Bot class for each server commands

--- AI/src/ai_zappy.py
+++ AI/src/ai_zappy.py
@@ -19,3 +19,38 @@ class Bot:
         self.team_name = team_name
         self.map = init_map(x, y)
         self.invantory = {'food' : 0, 'linemate' : 0, 'deraumere' : 0, 'sibur' : 0, 'mendiane' : 0, 'phiras' : 0, 'thystame' : 0}
+        self.position_x = 0
+        self.position_y = 0
+
+    def Forward(self):
+        return
+    
+    def Right(self):
+        return
+    
+    def Left(self):
+        return
+    
+    def Look(self):
+        return
+    
+    def Inventory(self):
+        return
+    
+    def Connect_nbr(self):
+        return
+    
+    def Fork(self):
+        return
+    
+    def Eject(self):
+        return
+    
+    def Take(self):
+        return
+    
+    def Set(self):
+        return
+    
+    def Incantation(self):
+        return
