8ed7046752e2ac664be5e101082fec2eff20b84a
julien <julien.renard@epitech.eu>
2024-06-03 14:55:40 +0200

    :pencil2: fix position and dimension variable type

--- AI/src/ai_zappy.py
+++ AI/src/ai_zappy.py
@@ -16,11 +16,10 @@ class Bot:
     def __init__(self, team_name, x, y):
         self.team_name = team_name
         self.map = init_map(x, y)
-        self.dimension = {'x' : x, 'y' : y}
+        self.dimension = {'x' : x - 1, 'y' : y - 1}
         self.inventory = {'food' : 0, 'linemate' : 0, 'deraumere' : 0, 'sibur' : 0, 'mendiane' : 0, 'phiras' : 0, 'thystame' : 0}
-        self.position_x = 0
-        self.position_y = 0
-        self.direction = 0
+        self.position = {'x' : 0, 'y' : 0}
+        self.direction = 
         self.level = 1
 
     def run(self):
@@ -32,22 +31,22 @@ class Bot:
         #function calling for connection
         if res == "ok":
             if self.direction == 1:
-                self.position_x += 1
+                self.position['x'] += 1
             if self.direction == 2:
-                self.position_y += 1
+                self.position['y'] += 1
             if self.direction == 3:
-                self.position_x -= 1
+                self.position['x'] -= 1
             if self.direction == 4:
-                self.position_y -= 1
+                self.position['y'] -= 1
 
-            if self.position_y >= len(self.map):
-                self.position_y == 0
-            if self.position_y < 0:
-                self.position_y == len(self.map) - 1
-            if self.position_x >= len(self.map[0]):
-                self.position_x == 0
-            if self.position_y < 0:
-                self.position_y == len(self.map[0]) - 1
+            if self.position['y'] > self.dimension['y']:
+                self.position['y'] == 0
+            if self.position['y'] < 0:
+                self.position['y'] == self.dimension['y']
+            if self.position['x'] >= self.dimension['x']:
+                self.position['x'] == 0
+            if self.position['x'] < 0:
+                self.position['x'] == self.dimension['x']
         return
     
     def right(self):
