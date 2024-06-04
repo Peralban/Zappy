b749a164122ad3b4a8bd87c25a7261b224e34ded
PerAlban <alban.peralta@epitech.eu>
2024-06-04 15:25:51 +0200

    :memo: Comment in_game struct

--- Network/include/Server/game.h
+++ Network/include/Server/game.h
@@ -7,17 +7,29 @@
 
 #pragma once
 
+/**
+ * @struct tile_s
+ * @brief Structure representing a tile in the game.
+ */
 typedef struct tile_s {
     item_t *items;
     drone_t *drone;
 } tile_t;
 
+/**
+ * @struct team_s
+ * @brief Structure representing a team in the game.
+ */
 typedef struct team_s {
     char *name;
     int connected_clients;
     drone_t *drone;
 } team_t;
 
+/**
+ * @struct in_game_s
+ * @brief Structure representing the state of the game.
+ */
 typedef struct in_game_s {
     tile_t **map;
     team_t *teams;
