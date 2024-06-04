c5908ce00fa23f9140984579a4337e9428a11c22
Antoinegtir <antoine.gonthier@epitech.eu>
2024-06-04 16:18:02 +0200

    🔥 externalize client part from the main.py by creating client_module

--- AI/src/main.py
+++ AI/src/main.py
@@ -1,26 +1,19 @@
-import socket
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+"""
+EPITECH PROJECT, 2024
+main.py
+Description:
+Retrieve value from arguments given by the user.
+"""
+
+from AI.src.client_module import connect_to_server
+from AI.src.parsing import parse_arguments
 
 def main():
-    server_host = '127.0.0.1'
-    server_port = 12345
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_host, server_port))
-    print("Connected to server.")
-
-    try:
-        while True:
-            message = input("Enter message: ")
-            if message.lower() == 'exit':
-                break
-            client_socket.sendall(message.encode())
-            response = client_socket.recv(1024).decode()
-            print("Server response:", response)
-    except KeyboardInterrupt:
-        print("\nClient interrupted.")
-    finally:
-        client_socket.close()
-        print("Disconnected from server.")
+    port, name, host = parse_arguments()
+    connect_to_server(host, port, name)
 
 if __name__ == "__main__":
     main()
