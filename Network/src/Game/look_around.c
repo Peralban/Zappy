47e65fcaee6a1658ea2507e9181d700704abc693
landryarki <landry.gigant@epitech.eu>
2024-06-10 16:17:10 +0200

    :bug: fix message of look to put comas

--- Network/src/Game/look_around.c
+++ Network/src/Game/look_around.c
@@ -13,19 +13,14 @@ static char *scan_tile(tile_t *tile)
     char *return_str = calloc(1, sizeof(char) * 1024);
     char *type_str[] = {"food", "linemate", "deraumere",
     "sibur", "mendiane", "phiras", "thystame"};
-    bool first = true;
 
     for (int i = 0; i < 7; i++) {
-        if (tile->inventory[i] == 0)
-            continue;
-        if (!first)
+        for (int y = 0; y < tile->inventory[i]; y++) {
+            strcat(return_str, type_str[i]);
             strcat(return_str, " ");
-        first = false;
-        for (int y = 0; y < tile->inventory[y]; y++)
-            strcat(return_str, type_str[tile->inventory[y]]);
+        }
     }
-    //if (tile->drone_list != NULL && strlen(return_str) != 0)
-    //    strcat(return_str, " ");
+    return_str[strlen(return_str) - 1] = '\0';
     for (linked_list_drone_t *tmp = tile->drone_list;
     tmp != NULL; tmp = tmp->next) {
         strcat(return_str, "player");
@@ -54,6 +49,7 @@ static char *look_at(drone_t *drone, server_t *server, int nb_observable,
         (ref[link[ori][1]] + j + max[link[ori][1]]) % max[link[ori][1]];
         tmp = scan_tile(&(server->game->map[new[X]][new[Y]]));
         strcat(return_str, tmp);
+        strcat(return_str, ",");
         free(tmp);
     }
     return return_str;
@@ -74,7 +70,7 @@ char *look_around(drone_t *drone, server_t *server)
         strcat(return_str, tmp);
         free(tmp);
         if (i != drone->level - 1)
-            strcat(return_str, ", ");
+            strcat(return_str, ",");
     }
     strcat(return_str, "]\n");
     return return_str;
