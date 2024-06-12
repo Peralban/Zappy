1f4852ec90b29b1ae831108b065a1250507e6d94
landryarki <landry.gigant@epitech.eu>
2024-06-12 02:48:25 +0200

    :sparkles: pnw event

--- Network/src/GuiProtocol/gui_event.c
+++ Network/src/GuiProtocol/gui_event.c
@@ -7,6 +7,17 @@
 
 #include "GuiProtocol/gui_event.h"
 
+void send_all_graphics(server_t *server, char *str)
+{
+    client_list_t *tmp = server->list;
+
+    while (tmp != NULL) {
+        if (tmp->client->state == GRAPHIC)
+            send(tmp->client->socket, str, strlen(str), 0);
+        tmp = tmp->next;
+    }
+}
+
 void gui_event(event_type_e event, server_t *server, void *data)
 {
     for (int i = 0; event_gui[i].event_type != 0; i++) {
@@ -15,4 +26,15 @@ void gui_event(event_type_e event, server_t *server, void *data)
             return;
         }
     }
+}
+
+void pnw(server_t *server, void *data)
+{
+    client_t *client = (client_t *)data;
+    char *str = malloc(sizeof(char) * 1024);
+
+    sprintf(str, "pnw %d %d %d %d %d %s\n", client->drone->id, client->drone->x,
+        client->drone->y, client->drone->orientation, client->drone->level, client->drone->team_name);
+    send_all_graphics(server, str);
+    free(str);
 }
