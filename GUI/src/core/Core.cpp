1ee6ffe42f533f8630ad8d9a5b1cd1752808916b
Antoine <antoine.gonthier@epitech.eu>
2024-06-19 13:57:01 +0200

    :sparkles: update Core for the new branch

--- GUI/src/core/Core.cpp
+++ GUI/src/core/Core.cpp
@@ -1,50 +1,73 @@
+/*
+** EPITECH PROJECT, 2024
+** GUI
+** File description:
+** Core
+*/
 #include "Core.hpp"
 #include <iostream>
 
-Core::Core(int argc, char **argv)
-    : m_window(1920, 1080, 30, 10, 15.0f, irr::video::EDT_OPENGL, MID, false)
+Core::Core(int width, int height, int platformX, int platformY, float tileSize, quality gameQuality, bool debug)
+    : m_window(width, height, irr::video::EDT_OPENGL, gameQuality, debug),
+      m_client(new guiNetworkClient()),
+      m_zappy(new ZappyGame())
 {
-    
-    this->m_argc = argc;
-    this->m_argv = argv;
-    this->m_client = nullptr;
-    this->m_zappy = nullptr;
+    m_zappy->setPlatformSize(platformX, platformY);
+    m_zappy->setTileSize(tileSize);
 }
 
-void Core::initialize()
+Core::~Core()
+{
+    delete m_client;
+    delete m_zappy;
+}
+
+void Core::initialize(int ac, char **av)
 {
     std::cout << "Starting Zappy" << std::endl;
-    m_client = new guiNetworkClient();
-    m_zappy = new ZappyGame();
 
-    m_window.parseArgs(m_argc, m_argv);
+    m_window.parseArgs(ac, av);
     m_window.windowCreateDevice();
 
     if (m_window.getDevice() == nullptr)
+    {
+        std::cerr << "Failed to create Irrlicht device!" << std::endl;
         std::exit(1);
+    }
 
     m_window.initDrivers();
     m_window.initLoader();
     m_window.initCamera();
-    m_window.initChessBoard();
     m_window.initEventReceiver();
 
+    std::cout << " -------------- LINKING CLIENT AND ZAPPY -------------- " << std::endl;
     m_window.linkZappyGame(m_zappy);
     m_window.linkGuiClient(m_client);
 
+    std::cout << " -------------- CREATING SOCKET -------------- " << std::endl;
     m_client->createSocket();
-    m_client->initIdentification();
-    std::cout << "map size is " << m_client->getMapSize() << std::endl;
-    std::cout << "time unit is " << m_client->getTimeUnit() << std::endl;
+
+    std::cout << " -------------- INITIALIZING IDENTIFICATION -------------- " << std::endl;
+    m_client->askInitData();
+
+    std::cout << " -------------- INITIALIZING CHESS BOARD -------------- " << std::endl;
     m_client->makeNonBlocking();
 
     m_zappy->loadChessPieces();
     m_zappy->addPlayer("player1");
     m_zappy->getPlayer("player1")->getPlayerPosition()->setPos(5, 3);
-    m_zappy->getPlayer("player1")->getPlayerPosition()->setConvertedZ(5.0f);
+    m_zappy->getPlayer("player1")->getPlayerPosition()->setZ(2);
     m_zappy->getPlayer("player1")->getPlayerPosition()->setOrientation(0, 0, 2);
     m_zappy->getPlayer("player1")->updatePlayerPos();
     m_zappy->getPlayer("player1")->setLevel(2);
+}
+
+Core::Core(int width, int height, int platformX, int platformY, float tileSize, quality gameQuality, bool debug)
+    : m_window(width, height, irr::video::EDT_OPENGL, gameQuality, debug),
+      m_client(new guiNetworkClient()),
+      m_zappy(new ZappyGame()) {
+    m_zappy->setPlatformSize(platformX, platformY);
+    m_zappy->setTileSize(tileSize);
 }
 
 void Core::run()
