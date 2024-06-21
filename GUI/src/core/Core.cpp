4ac1456ce0218bd43eee52a7353e56b4c73280d4
Antoine <antoine.gonthier@epitech.eu>
2024-06-21 11:19:37 +0200

    :sparkles: add way to catch error thrown properly from the errors classes

--- GUI/src/core/Core.cpp
+++ GUI/src/core/Core.cpp
@@ -26,40 +26,44 @@ void Core::initialize(int ac, char **av)
 {
     std::cout << "Starting Zappy" << std::endl;
 
-    m_window.parseArgs(ac, av);
-    m_window.windowCreateDevice();
+    try {
+        m_window.parseArgs(ac, av);
+        m_window.windowCreateDevice();
 
-    if (m_window.getDevice() == nullptr)
-    {
-        std::cerr << "Failed to create Irrlicht device!" << std::endl;
-        std::exit(1);
+        if (m_window.getDevice() == nullptr)
+        {
+            std::cerr << "Failed to create Irrlicht device!" << std::endl;
+            std::exit(1);
+        }
+
+        m_window.initDrivers();
+        m_window.initLoader();
+        m_window.initCamera();
+        m_window.initEventReceiver();
+
+        std::cout << " -------------- LINKING CLIENT AND ZAPPY -------------- " << std::endl;
+        m_window.linkZappyGame(m_zappy);
+        m_window.linkGuiClient(m_client);
+
+        std::cout << " -------------- CREATING SOCKET -------------- " << std::endl;
+        m_client->createSocket();
+
+        std::cout << " -------------- INITIALIZING IDENTIFICATION -------------- " << std::endl;
+        m_client->askInitData();
+
+        std::cout << " -------------- INITIALIZING CHESS BOARD -------------- " << std::endl;
+        m_client->makeNonBlocking();
+
+        m_zappy->loadChessPieces();
+        m_zappy->addPlayer("player1");
+        m_zappy->getPlayer("player1")->getPlayerPosition()->setPos(5, 3);
+        m_zappy->getPlayer("player1")->getPlayerPosition()->setZ(2);
+        m_zappy->getPlayer("player1")->getPlayerPosition()->setOrientation(0, 0, 2);
+        m_zappy->getPlayer("player1")->updatePlayerPos();
+        m_zappy->getPlayer("player1")->setLevel(2);
+    } catch (std::exception &e) {
+        exit(84);
     }
-
-    m_window.initDrivers();
-    m_window.initLoader();
-    m_window.initCamera();
-    m_window.initEventReceiver();
-
-    std::cout << " -------------- LINKING CLIENT AND ZAPPY -------------- " << std::endl;
-    m_window.linkZappyGame(m_zappy);
-    m_window.linkGuiClient(m_client);
-
-    std::cout << " -------------- CREATING SOCKET -------------- " << std::endl;
-    m_client->createSocket();
-
-    std::cout << " -------------- INITIALIZING IDENTIFICATION -------------- " << std::endl;
-    m_client->askInitData();
-
-    std::cout << " -------------- INITIALIZING CHESS BOARD -------------- " << std::endl;
-    m_client->makeNonBlocking();
-
-    m_zappy->loadChessPieces();
-    m_zappy->addPlayer("player1");
-    m_zappy->getPlayer("player1")->getPlayerPosition()->setPos(5, 3);
-    m_zappy->getPlayer("player1")->getPlayerPosition()->setZ(2);
-    m_zappy->getPlayer("player1")->getPlayerPosition()->setOrientation(0, 0, 2);
-    m_zappy->getPlayer("player1")->updatePlayerPos();
-    m_zappy->getPlayer("player1")->setLevel(2);
 }
 
 void Core::run()
