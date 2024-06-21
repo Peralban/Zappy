18da7737b8394a209e0101c8004263f5fc5ab446
Antoine <antoine.gonthier@epitech.eu>
2024-06-21 16:06:47 +0200

    :lipstick: fix space/indent

--- GUI/src/loader/textureLoader.cpp
+++ GUI/src/loader/textureLoader.cpp
@@ -19,7 +19,6 @@ TextureLoader::TextureLoader(irr::video::IVideoDriver *driver)
     _Driver = driver;
 }
 
-
 TextureLoader::~TextureLoader()
 {
 }
@@ -57,32 +56,28 @@ irr::video::ITexture *TextureLoader::loadTexture(const char *path)
     return texture;
 }
 
-irr::video::ITexture* TextureLoader::createTexture(int Red, int Green, int Blue, int Alpha)
+irr::video::ITexture *TextureLoader::createTexture(int Red, int Green, int Blue, int Alpha)
 {
     irr::u32 width = 256;
     irr::u32 height = 256;
     irr::video::IImage *tmpImage;
     irr::video::ITexture *tmpTexture;
     std::string name = "Texture";
+    irr::video::SColor color(Alpha, Red, Green, Blue);
     if (!_Driver) {
         throw NoVideoDriver();
     }
     tmpImage = _Driver->createImage(irr::video::ECF_A8R8G8B8, irr::core::dimension2d<irr::u32>(width, height));
     for (irr::u32 y = 0; y < height; ++y)
-    {
         for (irr::u32 x = 0; x < width; ++x)
-        {
-            irr::video::SColor color(Alpha, Red, Green, Blue);
             tmpImage->setPixel(x, y, color);
-        }
-    }
     name += std::to_string(Red) + std::to_string(Green) + std::to_string(Blue) + std::to_string(Alpha);
     tmpTexture = _Driver->addTexture(name.c_str(), tmpImage);
     tmpImage->drop();
     return tmpTexture;
 }
 
-irr::video::ITexture* TextureLoader::createGetTexture(int Red, int Green, int Blue, int Alpha, std::string name)
+irr::video::ITexture *TextureLoader::createGetTexture(int Red, int Green, int Blue, int Alpha, std::string name)
 {
     if (_Textures.find(name) != _Textures.end()) {
         return _Textures[name];
@@ -92,8 +87,7 @@ irr::video::ITexture* TextureLoader::createGetTexture(int Red, int Green, int Bl
     return texture;
 }
 
-
-irr::video::ITexture* TextureLoader::getTexture(std::string name)
+irr::video::ITexture *TextureLoader::getTexture(std::string name)
 {
     if (_Textures.find(name) == _Textures.end()) {
         throw NotFindTexture();
