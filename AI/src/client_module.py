705f1cda2a499159a095ce68d4ef6ec7165b7883
landryarki <landry.gigant@epitech.eu>
2024-06-23 17:53:02 +0200

    :zap: handle ctrl c

--- AI/src/client_module.py
+++ AI/src/client_module.py
@@ -49,38 +49,39 @@ def connect_to_server(host, port, name):
     inputs = [sock_file, sys.stdin]
     nb = 0
 
-    #try:
-    while True:
-        readable, _, _ = select.select(inputs, [], [], 0)
-        for s in readable:
-            if s is sock_file:
-                for message in s:
-                    current_line += 1
-                    if not message:
-                        print('Disconnected from server')
-                        sock.close()
-                        sys.exit()
-                    else:
-                        print(message.strip())
-                        parts = message.strip().split()
-                        if current_line == 2:
-                            if message.strip() == 'ko':
-                                sys.exit(84)
-                            else:
-                                nb = int(parts[0])
-                        if current_line == 3:
-                            LatLng = (int(parts[0]), int(parts[1]))
-                            Bot = ai_zappy.Bot(name, LatLng[0], LatLng[1], nb)
-                            Bot.run()
+    try:
+        while True:
+            readable, _, _ = select.select(inputs, [], [], 0)
+            for s in readable:
+                if s is sock_file:
+                    for message in s:
+                        current_line += 1
+                        if not message:
+                            print('Disconnected from server')
                             sock.close()
-                            sock_file.close()
-                            sys.exit(0)
-    #except KeyboardInterrupt:
-    #    print("\nClient interrupted.")
-    #    sock.close()
-    #    sock_file.close()
-    #    sys.exit(0)
-    #finally:
-    sock.close()
-    sock_file.close()
-    sys.exit(0)
+                            sys.exit()
+                        else:
+                            print(message.strip())
+                            parts = message.strip().split()
+                            if current_line == 2:
+                                if message.strip() == 'ko':
+                                    sys.exit(84)
+                                else:
+                                    nb = int(parts[0])
+                            if current_line == 3:
+                                LatLng = (int(parts[0]), int(parts[1]))
+                                Bot = ai_zappy.Bot(name, LatLng[0], LatLng[1], nb)
+                                Bot.run()
+                                sock.close()
+                                sock_file.close()
+                                sys.exit(0)
+
+    except KeyboardInterrupt:
+        print("\nClient interrupted.")
+        sock.close()
+        sock_file.close()
+        sys.exit(0)
+    finally:
+        sock.close()
+        sock_file.close()
+        sys.exit(0)
