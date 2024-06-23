6d70a3e4dad2d311bf7f7920865745001b794a09
landryarki <landry.gigant@epitech.eu>
2024-06-23 17:20:32 +0200

    :zap: send more data to gui client when connecting

--- Network/src/GuiProtocol/start_gui_client.c
+++ Network/src/GuiProtocol/start_gui_client.c
@@ -7,17 +7,45 @@
 
 #include "Server/server.h"
 
-void start_gui_client(server_t *server, client_t *client)
+static void send_all_eggs(server_t *server, client_t *client)
 {
     linked_list_egg_t *list = server->game->egg_list;
     char buffer[1024] = {0};
 
-    if (client->state != GRAPHIC)
-        return;
     while (list != NULL) {
         sprintf(buffer, "enw %d %d %d %d\n", list->egg->id, -1,
             list->egg->x, list->egg->y);
         send(client->socket, buffer, strlen(buffer), 0);
         list = list->next;
     }
+}
+
+static void send_all_players(server_t *server, client_t *client)
+{
+    client_list_t *tmp = server->list;
+    char buffer[1024] = {0};
+
+    while (tmp != NULL) {
+        if (tmp->client && tmp->client->state == PLAYING) {
+            sprintf(buffer, "pnw %d %d %d %d %d %s\n", tmp->client->drone->id,
+                tmp->client->drone->x, tmp->client->drone->y,
+                tmp->client->drone->orientation, tmp->client->drone->level,
+                tmp->client->drone->team_name);
+            send(client->socket, buffer, strlen(buffer), 0);
+        }
+        tmp = tmp->next;
+    }
+}
+
+void start_gui_client(server_t *server, client_t *client)
+{
+    char buffer[1024] = {0};
+
+    if (client->state != GRAPHIC)
+        return;
+    sprintf(buffer, "msz %d %d\n", server->info_game.width,
+        server->info_game.height);
+    send(client->socket, buffer, strlen(buffer), 0);
+    send_all_eggs(server, client);
+    send_all_players(server, client);
 }
